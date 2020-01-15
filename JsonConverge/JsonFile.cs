0cb99fbcfe510948388ed8df8670a8179fbc8c76
ClementFbt <clement.froz@gmail.com>
2020-01-15 12:57:42 -0500

    Ajoutez des fichiers projet.

---
+++ JsonConverge/JsonFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConverge
+{
+    public class JsonFile
+    {
+        public List<Participant> participants { get; set; }
+        public List<Message> messages { get; set; }
+        public string title { get; set; }
+    }
+
+    public class Message
+    {
+        public string sender_name { get; set; }
+        public long timestamp_ms { get; set; }
+        public string content { get; set; }
+        public Sticker sticker { get; set; }
+        public List<Photo> photos { get; set; }
+        public List<Gif> gifs { get; set; }
+        public List<Video> videos { get; set; }
+        public List<Reaction> reactions { get; set; }
+    }
+
+    public class Participant
+    {
+        public string name { get; set; }
+    }
+
+    public class Sticker
+    {
+        public string uri { get; set; }
+    }
+    public class Thumbnail
+    {
+        public string uri { get; set; }
+    }
+    public class Video
+    {
+        public string uri { get; set; }
+        public long creation_timestamp { get; set; }
+        public Thumbnail thumbnail { get; set; }
+    }
+
+    public class Gif
+    {
+        public string uri { get; set; }
+    }
+
+    public class Photo
+    {
+        public string uri { get; set; }
+        public long creation_timestamp { get; set; }
+    }
+
+    public class Reaction
+    {
+        public string reaction { get; set; }
+        public string Participant { get; set; }
+    }
+}
---
+++ JsonConverge/browserFiles.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConverge
+{
+    class BrowserFiles
+    {
+        JsonFile file;
+        List<TabResults> tabResults;
+
+        public BrowserFiles()
+        {
+            file = new JsonFile();
+            tabResults = new List<TabResults>();
+            createFile();
+        }
+        public List<TabResults> createFile()
+        {
+            int newMsg;
+            for (int i = 1; i <= 10; i++)
+            {
+                file = JObject.Parse(File.ReadAllText(@"C:\Users\Fouby\Desktop\ManonCourreges_w_aJxFlVQg\message_" + i+".json")).ToObject<JsonFile>();
+                if (tabResults.Count == 0)
+                {
+                    foreach (var item in file.participants)
+                    {
+                        tabResults.Add(new TabResults { nom = item.name, nbMessages = 0 });
+                    }
+                }
+                foreach (var participant in file.participants)
+                {
+                    newMsg = file.messages.Where(m => m.sender_name == participant.name && m.timestamp_ms > (long)1577982612000 && m.content != null).Count();
+                    tabResults.FirstOrDefault(t => t.nom == participant.name).nbMessages += newMsg;
+                }
+            }
+            Console.WriteLine("Nombre total : ");
+            foreach (var item in tabResults.OrderByDescending(x => x.nbMessages))
+            {
+                item.nom = item.nom.Replace("Ã©", "é");
+                Console.WriteLine(item.nom + ":" + item.nbMessages);
+            }
+            return tabResults;
+        }
+    }
+
+    public class TabResults
+    {
+        public string nom { get; set; }
+        public int nbMessages { get; set; }
+    }
+}
